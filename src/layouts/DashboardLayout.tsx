f80d3bf78edc7d63a9cbbd0bc6f71dff459fa229
BadhanCB <badhancsesastc112@gmail.com>
2024-02-22 13:29:41 +0600

    design basic structure of dashboard

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -4,100 +4,39 @@ import { Link, Outlet, useLocation } from "react-router-dom";
 
 const DashboardLayout = () => {
     const pathName = useLocation().pathname;
+    const dashboardOptions = [
+        { title: "Account", path: "/dashboard" },
+        { title: "Orders", path: "/dashboard/orders" },
+        { title: "Cart", path: "/dashboard/cart" },
+        { title: "Wishlist", path: "/dashboard/wishlist" },
+        { title: "Checkout", path: "/dashboard/checkout" },
+    ];
 
     return (
         <section className="wrapper grid grid-cols-12 gap-6 py-12 px-4 md:px-8 lg:px-12">
             <section className="col-span-12 md:col-span-3 lg:col-span-2">
                 <ul className="text-xl flex flex-col gap-4">
-                    <li>
-                        <Link
-                            className={`flex gap-1 items-center w-max ${
-                                pathName === "/dashboard" && "text-amber-500"
-                            }`}
-                            to="/dashboard"
-                        >
-                            <CgArrowRight
-                                className={`transition-transform origin-left ${
-                                    pathName === "/dashboard"
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
+                    {dashboardOptions.map((option) => (
+                        <li>
+                            <Link
+                                className={`flex gap-1 items-center w-max ${
+                                    pathName === option.path && "text-amber-500"
                                 }`}
-                            />
-                            <span className="animated-border-b">Accounts</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className={`flex gap-1 items-center w-max ${
-                                pathName === "/dashboard/orders" &&
-                                "text-amber-500"
-                            }`}
-                            to="/dashboard/orders"
-                        >
-                            <CgArrowRight
-                                className={`transition-transform origin-left ${
-                                    pathName === "/dashboard/orders"
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                            />
-                            <span className="animated-border-b">Orders</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className={`flex gap-1 items-center w-max ${
-                                pathName === "/dashboard/cart" &&
-                                "text-amber-500"
-                            }`}
-                            to="/dashboard/cart"
-                        >
-                            <CgArrowRight
-                                className={`transition-transform origin-left ${
-                                    pathName === "/dashboard/cart"
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                            />
-                            <span className="animated-border-b">Cart</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className={`flex gap-1 items-center w-max ${
-                                pathName === "/dashboard/wishlist" &&
-                                "text-amber-500"
-                            }`}
-                            to="/dashboard/wishlist"
-                        >
-                            <CgArrowRight
-                                className={`transition-transform origin-left ${
-                                    pathName === "/dashboard/wishlist"
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                            />
-                            <span className="animated-border-b">Wishlist</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            className={`flex gap-1 items-center w-max ${
-                                pathName === "/dashboard/checkout" &&
-                                "text-amber-500"
-                            }`}
-                            to="/dashboard/checkout"
-                        >
-                            <CgArrowRight
-                                className={`transition-transform origin-left ${
-                                    pathName === "/dashboard/checkout"
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                            />
-                            <span className="animated-border-b">Checkout</span>
-                        </Link>
-                    </li>
+                                to={option.path}
+                            >
+                                <CgArrowRight
+                                    className={`transition-transform origin-left ${
+                                        pathName === option.path
+                                            ? "scale-x-100"
+                                            : "scale-x-0"
+                                    }`}
+                                />
+                                <span className="animated-border-b">
+                                    {option.title}
+                                </span>
+                            </Link>
+                        </li>
+                    ))}
                 </ul>
             </section>
             <section className="col-span-12 md:col-span-9 lg:col-span-10 bg-gray-50 shadow-inner">
--- src/pages/UserAccount/UserAccount.tsx
+++ src/pages/UserAccount/UserAccount.tsx
@@ -1,7 +1,40 @@
+import useAuth from "../../hooks/useAuth";
+import placeholderPhoto from "../../assets/images/placeholder.svg";
+
 const UserAccount = () => {
+    const { user } = useAuth();
+    const { _id, name, userName, email, phone, address, photo } = user;
     return (
-        <div>
-            <h1>Overview</h1>
+        <div className="p-4">
+            <div className="flex items-center gap-4">
+                <div className="w-32 h-32 rounded-full overflow-hidden">
+                    <img
+                        src={
+                            photo?.data && photo.type
+                                ? `data:${photo?.type};base64 ${photo?.data}`
+                                : placeholderPhoto
+                        }
+                        alt={name}
+                        className="w-full h-full object-cover object-center"
+                    />
+                </div>
+                <div>
+                    <h3 className="text-3xl">{userName}</h3>
+                    <h4 className="text-xl">
+                        <span className="text-gray-400">Name:</span> {name}
+                    </h4>
+                    <p>
+                        <span className="text-gray-400">Email:</span> {email}
+                    </p>
+                    <p>
+                        <span className="text-gray-400">Phone:</span> {phone}
+                    </p>
+                    <p>
+                        <span className="text-gray-400">Address:</span>{" "}
+                        {address}
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
